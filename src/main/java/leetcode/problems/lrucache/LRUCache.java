3a60625c7ee2434c28b532d49bd4703da20d4de3
Ryan Scott <lunari72@outlook.com>
2025-09-22 15:42:08 -0400

    Removes temp comments and makes note to add javadocs later

--- src/main/java/leetcode/problems/lrucache/LRUCache.java
+++ src/main/java/leetcode/problems/lrucache/LRUCache.java
@@ -67,16 +67,6 @@ public class LRUCache {
   }
 
   private void evictLeastRecentlyUsedNode() {
-    /**
-     * Test Cases: 1. List contains only that node (this.first.getKey() == this.last.getKey() ==
-     * node.getKey()) [2] this.first = null; this.last = null;
-     *
-     * <p>2. List contains 2 nodes [1, 2] IntNode nodeToEvict = this.last; this.last =
-     * nodeToEvict.getPrev(); this.last.setNext(null); nodeToEvict.setPrev(null);
-     *
-     * <p>3. List contains 3 nodes [1, 3, 2] IntNode nodeToEvict = this.last; this.last =
-     * nodeToEvict.getPrev(); this.last.setNext(null); nodeToEvict.setPrev(null);
-     */
     if (this.capacity == 1) {
       this.first = null;
       this.last = null;
@@ -89,18 +79,6 @@ public class LRUCache {
   }
 
   private void addNodeToFront(final IntNode node) {
-    /**
-     * Test Cases: 1. List is empty (this.first == null && this.last == null) [] this.first = node;
-     * this.last = node;
-     *
-     * <p>2. List contains 1 element (this.first != null && this.last != null && this.first.getKey()
-     * == this.last.getKey()) [5] node.setNext(this.first) this.first.setPrev(node) this.first =
-     * node;
-     *
-     * <p>3. List contains 2 elements (this.first != null && this.last != null &&
-     * this.first.getKey() != this.last.getKey()) [2, 5] node.setNext(this.first);
-     * this.first.setPrev(node); this.first = node;
-     */
     if (this.first == null) {
       this.last = node;
     } else {
@@ -111,17 +89,6 @@ public class LRUCache {
   }
 
   private void moveNodeToFront(final IntNode node) {
-    /**
-     * Test Cases: 2: Moving node that is somewhere in the middle of the list (node.getPrev() !=
-     * null && node.getNext() != null) (Therefore, this.capacity >= 3, this.lruCache.size() == 3,
-     * this.lruTracker.size() == 3) //node.getPrev().setNext(node.getNext());
-     * //node.getNext().setPrev(node.getPrev());
-     *
-     * <p>node.setPrev(null); node.setNext(this.first); this.first = node;
-     *
-     * <p>3: Moving a node that is last in the list (this.last.getKey() == node.getKey())
-     * //node.getPrev().setNext(null); //this.last = node.getPrev();
-     */
     if (node.prev != null) {
       if (node.next != null) {
         node.prev.next = node.next;
@@ -194,7 +161,7 @@ public class LRUCache {
     LOGGER.debug(log.toString());
   }
 
-  private class IntNode {
+  private static class IntNode {
     int key;
     int value;
     IntNode prev;
